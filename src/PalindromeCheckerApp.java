9be9d6c2c9438685cf871a24c1f02c8e20e04c1f
Sakshi28738 <sakshisinha454@gmail.com>
2026-02-17 15:51:58 +0530

    Base application setup for main branch

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+// UseCase1PalindromeCheckerApp.java
+
+// Class acts as container for application logic
+public class PalindromeCheckerApp {
+
+    // Main method - entry point of Java application
+    public static void main(String[] args) {
+
+
+    }
+}
+
