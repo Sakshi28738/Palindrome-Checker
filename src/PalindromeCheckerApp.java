94d7de0405f0986b9da9b5501a11d03a7ac8cdbe
Sakshi28738 <sakshisinha454@gmail.com>
2026-02-17 15:59:38 +0530

    UC1-Application Entry and Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,17 @@
+// UseCase1PalindromeCheckerApp.java
+
+// Class acts as container for application logic
 public class PalindromeCheckerApp {
-    static void main() {
+
+    // Main method - entry point of Java application
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to Palindrome Checker Management System ");
+
+        System.out.println("Version : 1.0");
+
+        System.out.println("System Initialized successfully.");
 
     }
 }
